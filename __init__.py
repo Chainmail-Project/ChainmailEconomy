1f4362f96a95268802e573238687190f40ac2184
Riley Flynn <riley@rileyflynn.me>
2017-04-13 18:36:20 -0230

    Added method to get balance for player

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 
+from Chainmail.Player import Player
 from Chainmail.Plugin import ChainmailPlugin
 
 
@@ -9,6 +10,22 @@ class ChainmailEconomy(ChainmailPlugin):
         super().__init__(manifest, wrapper)
         self.db = sqlite3.connect(os.path.join(manifest["path"], "economy.db"))
         self.initialize_db()
+
+    def get_balance(self, player: Player) -> float:
+        """
+        Gets the balance of a player
+        :param player: The player to get the balance for
+        :return: Their balance
+        """
+        with self.db:
+            cursor = self.db.cursor()
+            cursor.execute("select balance from Balances where uuid = ?", (player.uuid, ))
+            balance = cursor.fetchone()
+            if balance is None:
+                cursor.execute("insert into Balances (uuid, balance) values (?, 0.0)", (player.uuid, ))
+                return float(0)
+            else:
+                return balance
 
     def initialize_db(self):
         with self.db:
