56c2b84729bc9b3b62393562355568d35ff09a53
Riley Flynn <riley@rileyflynn.me>
2017-04-13 18:28:01 -0230

    Began setting up the database

--- __init__.py
+++ __init__.py
@@ -1,5 +1,15 @@
+import os
+import sqlite3
+
 from Chainmail.Plugin import ChainmailPlugin
 
 
 class ChainmailEconomy(ChainmailPlugin):
-    pass
+    def __init__(self, manifest: dict, wrapper: "Wrapper.Wrapper") -> None:
+        super().__init__(manifest, wrapper)
+        self.db = sqlite3.connect(os.path.join(manifest["path"], "economy.db"))
+        self.initialize_db()
+
+    def initialize_db(self):
+        with self.db:
+            self.db.execute("create table if not exists Balances (uuid text, balance real)")
